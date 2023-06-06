1b6ae962a8dd128edb55e28da8b60fdb05ee4ed5
turing0 <turing175@gmail.com>
2023-06-06 17:59:24 +0800

    avatar

--- app/components/radix/Avatar.tsx
+++ app/components/radix/Avatar.tsx
@@ -11,14 +11,14 @@ const AvatarDemo = ({
 	alt: string;
 	initials: string;
 }) => (
-	<AvatarPrimitive.Root className="AvatarRoot inline-flex items-center justify-center overflow-hidden w-12 h-12 rounded-full bg-slate-900">
+	<AvatarPrimitive.Root className="AvatarRoot inline-flex items-center justify-center overflow-hidden w-12 h-12 rounded-full bg-white">
 		<AvatarPrimitive.Image
 			className="AvatarImage w-100 h-100 object-cover"
 			src={src}
 			alt={alt}
 		/>
 		<AvatarPrimitive.Fallback
-			className="AvatarFallback w-100 h-100 flex items-center justify-center text-base text-white leading-none font-semibold"
+			className="AvatarFallback w-100 h-100 flex items-center justify-center text-base text-black leading-none font-semibold"
 			delayMs={600}
 		>
 			{initials}
