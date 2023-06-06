3d2b8fcf7299f9733970230b6beeec85e876da77
turing0 <turing175@gmail.com>
2023-06-06 10:07:16 +0800

    input

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -19,33 +19,33 @@ const Input: React.FC<InputProps> = ({ placeholder, value, type = "text", onChan
             {label && <p className="text-xl text-white font-semibold mb-2">{label}</p>}
             {/* <div className="flex items-center bg-black border-2 border-neutral-800 rounded-md"> */}
             <div className={`flex items-center bg-black border-2 rounded-md ${isFocused ? 'border-sky-500' : 'border-neutral-800'}`}>
-            {/* <span className="text-lg text-neutral-400 p-4">https://</span> */}
-            <span className="text-lg text-neutral-400 pl-4 pr-0">https://</span>
-            <input
-                disabled={disabled}
-                onChange={onChange}
-                onKeyDown={onKeyDown}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                value={value}
-                placeholder={placeholder}
-                type={type}
-                className="
-                    w-full
-                    pt-4 pr-4 pb-4 pl-2
-                    text-lg 
-                    bg-black 
-                    border-neutral-800 
-                    rounded-md
-                    outline-none
-                    text-white
+                {/* <span className="text-lg text-neutral-400 p-4">https://</span> */}
+                <span className="text-lg text-neutral-400 pl-4 pr-0">https://</span>
+                <input
+                    disabled={disabled}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    value={value}
+                    placeholder={placeholder}
+                    type={type}
+                    className="
+                        w-full
+                        pt-4 pr-4 pb-4 pl-2
+                        text-lg 
+                        bg-black 
+                        border-0
+                        rounded-md
+                        outline-none
+                        text-white
 
-                    transition
-                    disabled:bg-neutral-900
-                    disabled:opacity-70
-                    disabled:cursor-not-allowed
-                "
-            />
+                        transition
+                        disabled:bg-neutral-900
+                        disabled:opacity-70
+                        disabled:cursor-not-allowed
+                    "
+                />
             </div>
         </div>
     );
