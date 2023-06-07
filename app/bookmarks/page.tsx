8f7f9d6d81a2ca73f652dfa77cd350c16bc5d872
turing0 <turing175@gmail.com>
2023-06-07 11:11:32 +0800

    bm

---
+++ app/bookmarks/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Header from "@/app/components/Header";
+import { ClipLoader } from "react-spinners";
+import Feed from "../components/posts/Feed";
+
+const BookmarksView = ( ) => {
+    // const router = useRouter();
+
+    // const { data: fetchedUser, isLoading } = useUser(server!, username!);
+    
+    // if (isLoading || !fetchedUser) {
+    //   return (
+    //     <div className="flex justify-center items-center h-full">
+    //       <ClipLoader color="lightblue" size={80} />
+    //     </div>
+    //   )
+    // }
+  
+    return (
+      <>
+        <Header showBackArrow title="Bookmarks" />
+        <Feed type="bookmarks" />
+      </>
+     );
+  }
+   
+  export default BookmarksView;
