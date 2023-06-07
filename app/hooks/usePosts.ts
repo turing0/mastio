358ffc425cf47dd4d013173649f4beccd38d4aef
turing0 <turing175@gmail.com>
2023-06-07 10:42:08 +0800

    cm

--- app/hooks/usePosts.ts
+++ app/hooks/usePosts.ts
@@ -14,14 +14,29 @@ const usePosts = (server?: string, userId?: string, type?: string, maxId?: strin
     if (!userId) {
         if (type==='home') {
             url = `https://${server}/api/v1/timelines/home?limit=30`;
+            if (maxId) {
+                url = url + `&max_id=${maxId}`;
+            }
         } else if (type === 'trends') {
             url = `https://${server}/api/v1/trends/statuses`;
+            if (offset) {
+                url = url + `?offset=${offset}`;
+            }
         } else if (type === 'local') {
             url = `https://${server}/api/v1/timelines/public?limit=30&local=true`;
+            if (maxId) {
+                url = url + `&max_id=${maxId}`;
+            }
         } else if (type === 'public') {
             url = `https://${server}/api/v1/timelines/public?limit=30`;
+            if (maxId) {
+                url = url + `&max_id=${maxId}`;
+            }
         } else if (type === 'favorites') {
             url = `https://${server}/api/v1/favourites`;
+            if (maxId) {
+                url = url + `?max_id=${maxId}`;
+            }
         } else if (type === 'bookmarks') {
             url = `https://${server}/api/v1/bookmarks`;
         } else { // default home
@@ -31,12 +46,12 @@ const usePosts = (server?: string, userId?: string, type?: string, maxId?: strin
     } else {
         url = `https://${server}/api/v1/accounts/${userId}/statuses?limit=30&exclude_replies=true`;
     }
-    if (maxId) {
-        url = url + `&max_id=${maxId}`;
-    }
-    if (offset) {
-        url = url + `?offset=${offset}`;
-    }
+    // if (maxId) {
+    //     url = url + `&max_id=${maxId}`;
+    // }
+    // if (offset) {
+    //     url = url + `?offset=${offset}`;
+    // }
     const { data, error, isLoading, mutate } = useSWR(
         server ? url : null, 
         // url, 
